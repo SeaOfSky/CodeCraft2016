9fe4a1af819f8f5ae04052a2af49a4cc0603e954
doodle777 <duanxu@outlook.com>
2016-05-15 12:28:45 +0800

    final build

---
+++ test.sh
@@ -0,0 +1,17 @@
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case0/topo.csv ~/ClionProjects/CodeCraft/test/case0/demand.csv ~/ClionProjects/CodeCraft/test/case0/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case1/topo.csv ~/ClionProjects/CodeCraft/test/case1/demand.csv ~/ClionProjects/CodeCraft/test/case1/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case2/topo.csv ~/ClionProjects/CodeCraft/test/case2/demand.csv ~/ClionProjects/CodeCraft/test/case2/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case3/topo.csv ~/ClionProjects/CodeCraft/test/case3/demand.csv ~/ClionProjects/CodeCraft/test/case3/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case4/topo.csv ~/ClionProjects/CodeCraft/test/case4/demand.csv ~/ClionProjects/CodeCraft/test/case4/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case5/topo.csv ~/ClionProjects/CodeCraft/test/case5/demand.csv ~/ClionProjects/CodeCraft/test/case5/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case6/topo.csv ~/ClionProjects/CodeCraft/test/case6/demand.csv ~/ClionProjects/CodeCraft/test/case6/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case7/topo.csv ~/ClionProjects/CodeCraft/test/case7/demand.csv ~/ClionProjects/CodeCraft/test/case7/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case8/topo.csv ~/ClionProjects/CodeCraft/test/case8/demand.csv ~/ClionProjects/CodeCraft/test/case8/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case9/topo.csv ~/ClionProjects/CodeCraft/test/case9/demand.csv ~/ClionProjects/CodeCraft/test/case9/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case10/topo.csv ~/ClionProjects/CodeCraft/test/case10/demand.csv ~/ClionProjects/CodeCraft/test/case10/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case11/topo.csv ~/ClionProjects/CodeCraft/test/case11/demand.csv ~/ClionProjects/CodeCraft/test/case11/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case12/topo.csv ~/ClionProjects/CodeCraft/test/case12/demand.csv ~/ClionProjects/CodeCraft/test/case12/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case13/topo.csv ~/ClionProjects/CodeCraft/test/case13/demand.csv ~/ClionProjects/CodeCraft/test/case13/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case14/topo.csv ~/ClionProjects/CodeCraft/test/case14/demand.csv ~/ClionProjects/CodeCraft/test/case14/result.csv
+~/ClionProjects/CodeCraft/bin/future_net ~/ClionProjects/CodeCraft/test/case15/topo.csv ~/ClionProjects/CodeCraft/test/case15/demand.csv ~/ClionProjects/CodeCraft/test/case15/result.csv
+
